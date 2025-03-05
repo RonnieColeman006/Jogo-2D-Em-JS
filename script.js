7c04251d05faf3972b14734e1dbacacef3d93e6e
GABRIEL FRANÇA ANDERSON <gabriel_anderson@estudante.sesisenai.org.br>
2025-03-05 10:15:01 -0300

    Terminando o JS

--- script.js
+++ script.js
@@ -2,7 +2,45 @@ const canvas = document.getElementById('Jogo2D')
 
 const ctx = canvas.getContext('2d')
 
-function loop (){
+document.addEventListener('keypress', (e) => {
+    if(e.code == 'Space'){
+        console.log('sim')
+        personagem.velocidadey = 15
+        personagem.pulando = true
+    }
+})
+
+const personagem = {
+    x:100,
+    y: canvas.height - 50,
+    altura: 50,
+    largura: 50,
+    velocidade:0, 
+    pulando: false
+}
+
+function desenharPersonagem(){
+ctx.fillRect(personagem.x, personagem.y, personagem.altura, personagem.largura)
+
+ctx.fillStyle = 'Black'
+}
+desenharPersonagem()
+
+function atualizarPersonagem(){
+
+    if(personagem.pulando == true){
+        personagem.y -= personagem.velocidadey
+    }
 
 }
- loop()
+
+function loop (){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+   
+    desenharPersonagem()
+
+    atualizarPersonagem()
+
+    requestAnimationFrame(loop)
+}
+loop()
